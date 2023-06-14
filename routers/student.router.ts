c184bf2fa980a9be34555e1a441824983a0ea7a5
kujawapatryk <homik363@wp.pl>
2023-06-14 20:48:51 +0200

    user retrieval refactoring

--- routers/student.router.ts
+++ routers/student.router.ts
@@ -3,6 +3,7 @@ import { StudentRecord } from '../records/student.record';
 import { StudentFilter } from '../records/student.filter';
 import { FilterQuery, UpdateStatus } from '../types';
 import multer from 'multer';
+import { convertTypes } from '../utils/convertTypes';
 
 const upload = multer({ dest: './utils/download/' })
 
@@ -10,30 +11,14 @@ export const studentRouter = Router();
 
 studentRouter
 
-    .get('/all', async (req, res) => {
-        const query= req.query as undefined as FilterQuery; //@TODO This is a strange solution, but I haven't found another way to make types work.
+    .get('/students', async (req, res) => {
+        const query= convertTypes(req.query) as FilterQuery;
         const availableStudents = new StudentFilter(query);
-        const data = await availableStudents.get();
+        const data = await availableStudents.getStudents();
         const allRecords = await availableStudents.allRecordsStudent();
         const newData = {
             allRecords: allRecords,
             data: data,
-        }
-
-        res.json(newData);
-    })
-
-    .get('/reserved/',async (req,res)=>{
-        console.log(req.query);
-        const query= req.query as undefined as FilterQuery; //@TODO This is a strange solution, but I haven't found another way to make types work.
-        const availableStudents = new StudentFilter(query);
-        console.log(availableStudents);
-        const data = await availableStudents.getReserved();
-        const allRecords = await availableStudents.allRecordsReservedStudent();
-        console.log({ allRecords });
-        const newData = {
-            allRecords,
-            data,
         }
 
         res.json(newData);
@@ -49,16 +34,12 @@ studentRouter
         const studentId = req.params.studentId;
         const data = await StudentRecord.getCvOneStudent(studentId);
         res.json(data);
-    // wymaga id studenta i zwraca wszystkie dane wymagane do wyświetlenia cv
-    // kursanta (makieta 6)
     })
 
     .get('/getcvedit/:studentId', async (req, res) => {
         const studentId = req.params.studentId;
         const data = await StudentRecord.getCvOneStudentEdit(studentId);
         res.json(data);
-    // wymaga id studenta i zwraca wszystkie dane wymagane do wyświetlenia cv
-    // kursanta (makieta 6)
     })
 
     .patch('/changedata', async (req, res) => {
@@ -67,15 +48,11 @@ studentRouter
         res.json(data);
     })
 
-    .get('/test', (req, res) => {
-        res.send({
-            be: 'is working 🥳',
-        });
-    })
     .post('/newstudents', upload.single('dataFile'), async (req, res) => {
         await StudentRecord.addNewStudent(req.file.filename);
         res.status(200).json({ success: true, message: 'Kursanci zostali dodani' });
     })
+
     .get('/name/:id', async (req, res) => {
         const studentId = req.params.id;
         const { firstName, lastName, githubUsername } = (await StudentRecord.getCvOneStudent(studentId));
