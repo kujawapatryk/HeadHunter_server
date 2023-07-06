22bfac13d0c10892543be8937baf6c88be88bf27
kujawapatryk <homik363@wp.pl>
2023-07-06 03:35:36 +0200

    types employed students.

---
+++ types/student/employed.ts
@@ -0,0 +1,9 @@
+export interface Employed{
+    studentId: string,
+    firstName: string,
+    lastName: string,
+    githubUsername: string,
+    reservedBy: string,
+    fullName: string,
+    company: string,
+}
