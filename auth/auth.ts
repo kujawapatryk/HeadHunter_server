190d0fe55ac91d4e48f57cf60c1e9890e51e9a64
kujawapatryk <homik363@wp.pl>
2023-07-16 02:37:18 +0200

    Improving error handling.

--- auth/auth.ts
+++ auth/auth.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { UserRecord } from '../records/user.record';
 import { config } from '../config/config';
-import { ValidationError } from '../utils/errors';
 import { UserEntity } from '../types';
 
 type DecodedToken = {
@@ -15,19 +14,20 @@ export const auth = (states: number[]) => async  (req: Request, res: Response, n
 
     try {
         const { token }  = req.cookies;
-        if (!token) return res.status(403).json({ message: 'Nie znaleziono tokena.' });
+        console.log(token)
+        if (!token) return res.status(403).json({ message: 'unAuthorized' });
 
         jwt.verify(token, config.JWT_SECRET, async (err: jwt.VerifyErrors) => {
-            if (err) return res.status(403).send('Nieprawidłowy token.');
+            if (err) return res.status(403).json({ message: 'unAuthorized' });
         });
 
         const decoded: DecodedToken = jwt.verify(token, config.JWT_SECRET) as DecodedToken;
         const user: UserEntity = await UserRecord.getEmail(decoded.id);
 
-        if (!user) return res.status(401).send('Nieautoryzowany użytkownik.');
+        if (!user) return res.status(401).json({ message: 'unAuthorized' });
 
         if(states.indexOf(user.userState) === -1)
-            throw new ValidationError('unAuthorized');
+            return res.status(401).json({ message: 'unAuthorized' });
 
         req.user = user;
         next();
